f1c823781fdd952d6241cb0be54ca229119677c3
nechanya <aabushmeleva@edu.hse.ru>
2018-11-16 15:47:24 +0300

    добавили user

--- GetFit.py
+++ GetFit.py
@@ -16,6 +16,10 @@ def anketa():
 def selecting():
     return 'Wait a second: we are selecting the participants for your team!'
 
+@app.route('/user/<username>')
+def user_page(username):
+    return render_template('page0.html', user=username)
+
 if __name__== '__main__':
     app.run()
 
