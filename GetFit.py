c10edd9c1ffe18a701d129ea4736c696aa17c025
YarovayaPolina <43449873+YarovayaPolina@users.noreply.github.com>
2018-12-06 14:36:18 +0300

    Update GetFit.py

--- GetFit.py
+++ GetFit.py
@@ -1,9 +1,20 @@
 from flask import Flask
 from flask import render_template
 import db
+import sqlite3
 from flask import request
 
-app = Flask(__name__)\
+app = Flask(__name__)
+
+@app.route('/list')
+def user_list():
+    conn = sqlite3.connect('new_db')
+    c=conn.cursor()
+    c.execute("SELECT * FROM info_about_users ")
+    users=list(c.fetchall())
+    conn.close()
+    return render_template('page01.html', users=users)
+
 
 @app.route('/')
 def first():
@@ -18,7 +29,7 @@ def sign_up():
     return render_template('get_fit_registration.html')
 
 @app.route('/personal')
-def second():
+def personal():
     return render_template('get_fit_personal.html')
 
 
@@ -26,22 +37,22 @@ def second():
 def diary():
     return render_template('page_diary.html')
 
+@app.route('/grouppage')
+def grouppage():
+    return render_template('get_fit_grouppage.html')
 
 
 @app.route('/result')
 def search_for_person():
     q=request.args.get('query')
     users = db.get_users_by_name(q)
-    return render_template('getfit_anketa.html', q=q, users=users)
+    return render_template('page01.html', q=q, users=users)
 
 
-@app.route('/selecting')
-def selecting():
-    return 'Wait a second: we are selecting the participants for your team!'\
 
 @app.route('/user/<username>')
 def get_user(username):
-    return render_template('getfit_anketa.html', name=username)
+    return render_template('page01.html', name=username)
 
 if __name__ == "__main__":
     app.run()
